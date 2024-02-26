c2582bdd20854ac526791788910b384066409afd
ra2357028 <ra2357028@LDI304>
2024-02-26 09:21:57 -0300

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,17 @@
+﻿double[] salarios = new double[5];
+string? j;
+for (int i = 0; i < salarios.Length; i++)
+{
+    Console.Write((i + 1) + "º salário:");
+    j = Console.ReadLine();
+    Double.TryParse(j, out salarios[i]);
+    if (salarios[i] < 1000)
+    {
+        salarios[i] = salarios[i] * 1.1;
+    }
+}
+Console.WriteLine();
+for (int n = 0; n < salarios.Length; n++)
+{
+    Console.WriteLine((n + 1) + "º salário: R$" + salarios[n].ToString("0.00"));
+}
